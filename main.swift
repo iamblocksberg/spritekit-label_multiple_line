1669f65956da6d64d4b58c66c4ef48244865101d
iamblocksberg <8385967+iamblocksberg@users.noreply.github.com>
2019-12-06 22:09:04 +0700

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,5 @@
+let label = self.childNode(withName: "LabelName") as? SKLabelNode
+label.text = "Lorem ~ jaawdawdsawdo8"
+label.lineBreakMode = NSLineBreakMode.byWordWrapping
+label.numberOfLines = 0
+label.preferredMaxLayoutWidth = 500
